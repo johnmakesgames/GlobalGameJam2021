49325638333b83da85b90abd022bf18d8052531d
Angharad Hill <angharadjanehill@gmail.com>
2021-01-27 22:52:48 +0000

    Movement
    WASD/Arrow key movement set up, camera follow
    TO DO:
    fix camera rotation, add player rotating

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public float PlayerSpeedModifier = 0.1f;
+    private CharacterController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // PhysicsComponent = GetComponent<Rigidbody>();
+        playerController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 newVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        newVelocity *= PlayerSpeedModifier;
+        playerController.Move(newVelocity);
+       // PhysicsComponent.velocity = newVelocity;
+    }
+}
