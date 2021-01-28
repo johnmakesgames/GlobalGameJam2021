0a77bdf5160b537d37d0be555b76ed98a906a5d8
Angharad Hill <angharadjanehill@gmail.com>
2021-01-28 18:59:52 +0000

    Dog Walk

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,11 +15,15 @@ public class PlayerMovement : MonoBehaviour
     private bool playerDig = false;
     private bool playerShake = false;
     private Vector3 playerVelocity;
+    private enum AnimationState { Idle, Walking, Running, Shaking, Digging, Jumping, Attacking, Sniffing, WaggingTail, Sleeping, Petting }
+    AnimationState CurrentState;
+    AnimationState PreviousState;
 
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<CharacterController>();
+        CurrentState = AnimationState.Idle;
     }
 
     private void FixedUpdate()
@@ -30,41 +34,95 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-            playerGrounded = playerController.isGrounded;
-            if (playerGrounded && playerVelocity.y < 0)
-            {
-                playerVelocity.y = 0f;
-            }
-
-            Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * PlayerSpeedModifier;
-            playerController.Move(move);
-
-            if (move != Vector3.zero)
-            {
-                transform.forward = move;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space) && playerGrounded)
-            {
-                playerVelocity.y += Mathf.Sqrt(jumpHeight * -1.0f * -9.8f);
-            }
-
-            playerVelocity.y += -9.8f * Time.deltaTime;
-            playerController.Move(playerVelocity);
-
-            if(Input.GetKey(KeyCode.E)) //hold key down
-            {
-                //Play Dig Animation
-                playerAnimating = true;
-                playerAnimator.SetTrigger("Shaking");
-            }
-            else
-            {
-                playerAnimator.ResetTrigger("Shaking");
-            }
-       
+        PreviousState = CurrentState;
         
+        playerGrounded = playerController.isGrounded;
+        if (playerGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * PlayerSpeedModifier;
+        playerController.Move(move);
+
+        if (move != Vector3.zero)
+        {
+            transform.forward = move;
+            //base on speed, walking or running
+            CurrentState = AnimationState.Walking;
+            playerAnimator.SetTrigger("Walking");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && playerGrounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -1.0f * -9.8f);
+            CurrentState = AnimationState.Jumping;
+        }
+
+        playerVelocity.y += -9.8f * Time.deltaTime;
+        playerController.Move(playerVelocity);
+
+        if (Input.GetKey(KeyCode.Alpha1)) //hold key down
+        {
+
+            playerAnimator.SetTrigger("Shaking");
+            CurrentState = AnimationState.Shaking;
+        }
+        else
+        {
+            playerAnimator.ResetTrigger("Shaking");
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2)) //hold key down
+        {
+            playerAnimator.SetTrigger("Digging");
+            CurrentState = AnimationState.Digging;
+        }
+        else
+        {
+            playerAnimator.ResetTrigger("Digging");
+        }
+
+        if (Input.GetKey(KeyCode.Alpha3)) //hold key down
+        {
+            playerAnimator.SetTrigger("Attacking");
+            CurrentState = AnimationState.Attacking;
+        }
+        else
+        {
+            playerAnimator.ResetTrigger("Attacking");
+        }
+
+        if (Input.GetKey(KeyCode.Alpha4)) //hold key down
+        {
+            playerAnimator.SetTrigger("Sniffing");
+            CurrentState = AnimationState.Sniffing;
+        }
+        else
+        {
+            playerAnimator.ResetTrigger("Sniffing");
+        }
+
+        if (Input.GetKey(KeyCode.Alpha5)) //hold key down
+        {
+            playerAnimator.SetTrigger("Petting");
+            CurrentState = AnimationState.Petting;
+        }
+        else
+        {
+            playerAnimator.ResetTrigger("Petting");
+        }
+
+        if (Input.GetKey(KeyCode.Alpha6)) //hold key down
+        {
+            playerAnimator.SetTrigger("WaggingTail");
+            CurrentState = AnimationState.WaggingTail;
+        }
+        else
+        {
+            playerAnimator.ResetTrigger("WaggingTail");
+        }
+        // Action: Sniff, Shake, WagTail(maybe), (being)Pet , Attack, digging
+        // Movement: Walk, Idle, Run(maybe),  Sleep(AFK)
     }
 }
