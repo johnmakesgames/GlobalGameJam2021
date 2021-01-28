19036337ea8e2a817baf87f99baf791d7dace2ae
johnmakesgames <john.whiles@hotmail.com>
2021-01-28 13:05:48 +0000

    Cars and boats drive around passively

---
+++ Assets/Scripts/AI/AITypes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AITypes
+{
+    CAR,
+    BOAT,
+    PERSON
+}
--- Assets/Scripts/AI/NavMeshFollow.cs
+++ Assets/Scripts/AI/NavMeshFollow.cs
@@ -3,18 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(AgentController))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class NavMeshFollow : MonoBehaviour
 {
     public Vector3 targetLocation;
     private NavMeshAgent navMeshAgent;
+    private AgentController agentController;
+    float timesincerefresh;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = this.GetComponent<NavMeshAgent>();
-        if (navMeshAgent != null)
+        agentController = this.GetComponent<AgentController>();
+
+        GameObject[] locations;
+        switch (agentController.AiType)
         {
-            navMeshAgent.SetDestination(targetLocation);
+            case AITypes.PERSON:
+                break;
+            case AITypes.CAR:
+                navMeshAgent.speed = 10;
+                navMeshAgent.angularSpeed = 250;
+                navMeshAgent.acceleration = 8;
+                navMeshAgent.autoBraking = true;
+                locations = GameObject.FindGameObjectsWithTag("DriveToLocation");
+                targetLocation = locations[Random.Range(0, locations.Length)].GetComponent<Transform>().position;
+                timesincerefresh = 0;
+                break;
+            case AITypes.BOAT:
+                navMeshAgent.speed = 30;
+                navMeshAgent.angularSpeed = 80;
+                navMeshAgent.acceleration = 8;
+                navMeshAgent.autoBraking = true;
+                locations = GameObject.FindGameObjectsWithTag("Boat");
+                targetLocation = locations[Random.Range(0, locations.Length)].GetComponent<Transform>().position;
+                timesincerefresh = 0;
+                break;
+            default:
+                break;
+        }
+
+        navMeshAgent.SetDestination(targetLocation);
+    }
+
+    private void Update()
+    {
+        timesincerefresh += Time.deltaTime;
+
+        if (Vector3.Distance(targetLocation, this.GetComponent<Transform>().position) < 10 || timesincerefresh > 5)
+        {
+            GameObject[] locations;
+            switch (agentController.AiType)
+            {
+                case AITypes.PERSON:
+                    break;
+                case AITypes.CAR:
+                    navMeshAgent.speed = 10;
+                    navMeshAgent.angularSpeed = 250;
+                    navMeshAgent.acceleration = 8;
+                    navMeshAgent.autoBraking = true;
+                    locations = GameObject.FindGameObjectsWithTag("DriveToLocation");
+                    targetLocation = locations[Random.Range(0, locations.Length)].GetComponent<Transform>().position;
+                    timesincerefresh = 0;
+                    break;
+                case AITypes.BOAT:
+                    navMeshAgent.speed = 30;
+                    navMeshAgent.angularSpeed = 80;
+                    navMeshAgent.acceleration = 8;
+                    navMeshAgent.autoBraking = true;
+                    locations = GameObject.FindGameObjectsWithTag("Boat");
+                    targetLocation = locations[Random.Range(0, locations.Length)].GetComponent<Transform>().position;
+                    timesincerefresh = 0;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
