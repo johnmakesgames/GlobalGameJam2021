fbb9a1f7d644a595b7d33e5439a5f427fb67ea28
Angharad Hill <angharadjanehill@gmail.com>
2021-01-29 21:45:23 +0000

    In Editor Animation State Machine
    To Do: In Script condition setting

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     private bool playerDig = false;
     private bool playerShake = false;
     private Vector3 playerVelocity;
-    private enum AnimationState { Idle, Walking, Running, Shaking, Digging, Jumping, Attacking, Sniffing, WaggingTail, Sleeping, Petting }
+    private enum AnimationState { Idle, Walking, Running, Shaking, Digging, Jumping, Attacking, Sniffing, WaggingTail, Sleeping, Petting, Falling, DefaultState }
     AnimationState CurrentState;
     AnimationState PreviousState;
 
@@ -24,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
     {
         playerController = GetComponent<CharacterController>();
         CurrentState = AnimationState.Idle;
+        PreviousState = AnimationState.DefaultState;
     }
 
     private void FixedUpdate()
@@ -34,8 +35,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PreviousState = CurrentState;
-        
         playerGrounded = playerController.isGrounded;
         if (playerGrounded && playerVelocity.y < 0)
         {
@@ -50,8 +49,10 @@ public class PlayerMovement : MonoBehaviour
             transform.forward = move;
             //base on speed, walking or running
             CurrentState = AnimationState.Walking;
-            playerAnimator.SetTrigger("Walking");
+            //playerAnimator.ResetTrigger("Idle");
+            //playerAnimator.SetTrigger("Walking");
         }
+
 
         if (Input.GetKeyDown(KeyCode.Space) && playerGrounded)
         {
@@ -60,69 +61,213 @@ public class PlayerMovement : MonoBehaviour
         }
 
         playerVelocity.y += -9.8f * Time.deltaTime;
+
+        //if (playerVelocity.y < 0.0f)
+            //CurrentState = AnimationState.Falling;
+
         playerController.Move(playerVelocity);
 
-        if (Input.GetKey(KeyCode.Alpha1)) //hold key down
-        {
 
-            playerAnimator.SetTrigger("Shaking");
-            CurrentState = AnimationState.Shaking;
-        }
-        else
+        if(CurrentState != PreviousState)
         {
-            playerAnimator.ResetTrigger("Shaking");
-        }
+            switch (CurrentState)
+            {
+                case AnimationState.Walking:
+                    playerAnimator.ResetTrigger("Idle");
+                    playerAnimator.SetTrigger("Walking");
+                    break;
 
-        if (Input.GetKey(KeyCode.Alpha2)) //hold key down
-        {
-            playerAnimator.SetTrigger("Digging");
-            CurrentState = AnimationState.Digging;
-        }
-        else
-        {
-            playerAnimator.ResetTrigger("Digging");
-        }
+                case AnimationState.Idle:
+                    playerAnimator.ResetTrigger("Walking");
+                    playerAnimator.SetTrigger("Idle");
+                    break;
 
-        if (Input.GetKey(KeyCode.Alpha3)) //hold key down
-        {
-            playerAnimator.SetTrigger("Attacking");
-            CurrentState = AnimationState.Attacking;
-        }
-        else
-        {
-            playerAnimator.ResetTrigger("Attacking");
+                case AnimationState.Jumping:
+                    break;
+
+                case AnimationState.Falling:
+                    break;
+            }
         }
 
-        if (Input.GetKey(KeyCode.Alpha4)) //hold key down
-        {
-            playerAnimator.SetTrigger("Sniffing");
-            CurrentState = AnimationState.Sniffing;
-        }
-        else
-        {
-            playerAnimator.ResetTrigger("Sniffing");
-        }
+        Debug.Log(CurrentState);
 
-        if (Input.GetKey(KeyCode.Alpha5)) //hold key down
-        {
-            playerAnimator.SetTrigger("Petting");
-            CurrentState = AnimationState.Petting;
-        }
-        else
-        {
-            playerAnimator.ResetTrigger("Petting");
-        }
+        PreviousState = CurrentState;
 
-        if (Input.GetKey(KeyCode.Alpha6)) //hold key down
-        {
-            playerAnimator.SetTrigger("WaggingTail");
-            CurrentState = AnimationState.WaggingTail;
-        }
-        else
-        {
-            playerAnimator.ResetTrigger("WaggingTail");
-        }
         // Action: Sniff, Shake, WagTail(maybe), (being)Pet , Attack, digging
         // Movement: Walk, Idle, Run(maybe),  Sleep(AFK)
+
+        //if (CurrentState != PreviousState)
+        //{
+            
+        //    switch (PreviousState)
+        //    {
+        //        case AnimationState.Walking:
+        //            playerAnimator.ResetTrigger("Walking");
+        //            break;
+
+        //        case AnimationState.Running:
+        //            playerAnimator.ResetTrigger("Running");
+        //            break;
+
+        //        case AnimationState.Jumping:
+        //            playerAnimator.ResetTrigger("Jumping");
+        //            break;
+
+        //        case AnimationState.Falling:
+        //            playerAnimator.ResetTrigger("Falling");
+        //            break;
+
+        //        case AnimationState.Idle:
+        //            playerAnimator.ResetTrigger("Idle");
+        //            break;
+
+        //        case AnimationState.Digging:
+        //            playerAnimator.ResetTrigger("Digging");
+        //            break;
+
+        //        case AnimationState.Attacking:
+        //            playerAnimator.ResetTrigger("Attacking");
+        //            break;
+
+        //        case AnimationState.Petting:
+        //            playerAnimator.ResetTrigger("Petting");
+        //            break;
+
+        //        case AnimationState.Shaking:
+        //            playerAnimator.ResetTrigger("Shaking");
+        //            break;
+
+        //        case AnimationState.Sleeping:
+        //            playerAnimator.ResetTrigger("Sleeping");
+        //            break;
+
+        //        case AnimationState.Sniffing:
+        //            playerAnimator.ResetTrigger("Sniffing");
+        //            break;
+
+        //        case AnimationState.WaggingTail:
+        //            playerAnimator.ResetTrigger("WaggingTail");
+        //            break;
+        //    }
+
+        //    switch (CurrentState)
+        //    {
+        //        case AnimationState.Walking:
+        //            playerAnimator.SetTrigger("Walking");
+        //            break;
+
+        //        case AnimationState.Running:
+        //            playerAnimator.SetTrigger("Running");
+        //            break;
+
+        //        case AnimationState.Jumping:
+        //            playerAnimator.SetTrigger("Jumping");
+        //            break;
+
+        //        case AnimationState.Falling:
+        //            playerAnimator.SetTrigger("Falling");
+        //            break;
+
+        //        case AnimationState.Idle:
+        //            playerAnimator.SetTrigger("Idle");
+        //            break;
+
+        //        case AnimationState.Digging:
+        //            playerAnimator.SetTrigger("Digging");
+        //            break;
+
+        //        case AnimationState.Attacking:
+        //            playerAnimator.SetTrigger("Attacking");
+        //            break;
+
+        //        case AnimationState.Petting:
+        //            playerAnimator.SetTrigger("Petting");
+        //            break;
+
+        //        case AnimationState.Shaking:
+        //            playerAnimator.SetTrigger("Shaking");
+        //            break;
+
+        //        case AnimationState.Sleeping:
+        //            playerAnimator.SetTrigger("Sleeping");
+        //            break;
+
+        //        case AnimationState.Sniffing:
+        //            playerAnimator.SetTrigger("Sniffing");
+        //            break;
+
+        //        case AnimationState.WaggingTail:
+        //            playerAnimator.SetTrigger("WagginTail");
+        //            break;
+        //    }
+
+        //}
+
+        //PreviousState = CurrentState;
+
+        //Action Based Animations
+
+        //if (Input.GetKey(KeyCode.Alpha1)) //hold key down
+        //{
+
+        //    playerAnimator.SetTrigger("Shaking");
+        //    CurrentState = AnimationState.Shaking;
+        //}
+        //else
+        //{
+        //    playerAnimator.ResetTrigger("Shaking");
+        //}
+
+        //if (Input.GetKey(KeyCode.Alpha2)) //hold key down
+        //{
+        //    playerAnimator.SetTrigger("Digging");
+        //    CurrentState = AnimationState.Digging;
+        //}
+        //else
+        //{
+        //    playerAnimator.ResetTrigger("Digging");
+        //}
+
+        //if (Input.GetKey(KeyCode.Alpha3)) //hold key down
+        //{
+        //    playerAnimator.SetTrigger("Attacking");
+        //    CurrentState = AnimationState.Attacking;
+        //}
+        //else
+        //{
+        //    playerAnimator.ResetTrigger("Attacking");
+        //}
+
+        //if (Input.GetKey(KeyCode.Alpha4)) //hold key down
+        //{
+        //    playerAnimator.SetTrigger("Sniffing");
+        //    CurrentState = AnimationState.Sniffing;
+        //}
+        //else
+        //{
+        //    playerAnimator.ResetTrigger("Sniffing");
+        //}
+
+        //if (Input.GetKey(KeyCode.Alpha5)) //hold key down
+        //{
+        //    playerAnimator.SetTrigger("Petting");
+        //    CurrentState = AnimationState.Petting;
+        //}
+        //else
+        //{
+        //    playerAnimator.ResetTrigger("Petting");
+        //}
+
+        //if (Input.GetKey(KeyCode.Alpha6)) //hold key down
+        //{
+        //    playerAnimator.SetTrigger("WaggingTail");
+        //    CurrentState = AnimationState.WaggingTail;
+        //}
+        //else
+        //{
+        //    playerAnimator.ResetTrigger("WaggingTail");
+        //}
+
     }
 }
