92b429459b080ec874bb5016c947d65de4719706
Rhiannon <p018236h@student.staffs.ac.uk>
2021-01-31 13:19:03 +0000

    :sparkles: Gravestones Added :sparkles:

--- Assets/Scripts/PlayerGrave.cs
+++ Assets/Scripts/PlayerGrave.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerGrave : MonoBehaviour
 {
-    public GameObject[] deathType; 
     Respawner respawner;
+    [SerializeField]
+    int graveIndex;
+
+    [SerializeField]
+    public GameObject graveText;
+
+    private TextMeshPro text;
+
+    private void Start()
+    {
+        respawner = GameObject.FindGameObjectWithTag("Player").GetComponent<Respawner>();
+        respawner.deadCallback += AddToGravestone;
+    }
 
     void AddToGravestone()
     {
-        
+        if(respawner.deaths.Count > graveIndex)
+        {
+           graveText.GetComponent<TextMeshProUGUI>().SetText(respawner.deaths[graveIndex]);
+        }
     }
 }
