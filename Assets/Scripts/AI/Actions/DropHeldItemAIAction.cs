8937e6b7c068af2a67e8a1feb800e3cfb7db36f6
Samuel Johnson <sam.ross.johnson@gmail.com>
2021-01-31 12:20:15 +0000

    AI characters now drop held items when reaching max fear/awe :sparkles:

---
+++ Assets/Scripts/AI/Actions/DropHeldItemAIAction.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GGJ 2021/AI/Action/Drop Held Item")]
+public class DropHeldItemAIAction : AIAction
+{
+    private AIHeldItem heldItemComponent = null;
+
+    public override void Initialize(CreatureAIController controller)
+    {
+        base.Initialize(controller);
+
+        heldItemComponent = controller.GetComponent<AIHeldItem>();
+    }
+
+    public override void Act(CreatureAIController controller)
+    {
+        if(heldItemComponent.IsHoldingItem)
+        {
+            heldItemComponent.DropHeldItem();
+        }
+    }
+}
