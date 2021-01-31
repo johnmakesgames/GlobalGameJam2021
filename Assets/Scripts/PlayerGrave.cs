c9e3a5f100f421d2e7cec68404e3de49138668ef
Rhiannon <p018236h@student.staffs.ac.uk>
2021-01-31 11:57:33 +0000

    Basic :sparkles:

---
+++ Assets/Scripts/PlayerGrave.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrave : MonoBehaviour
+{
+    public GameObject[] deathType; 
+    Respawner respawner;
+
+    void AddToGravestone()
+    {
+        
+    }
+}
