d9e9a696afdce266faa80e111d6e03c7539c3263
johnmakesgames <john.whiles@hotmail.com>
2021-01-31 14:27:43 +0000

    BoneCounter UI added

---
+++ Assets/Scripts/UI/UpdateBoneTextUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateBoneTextUI : MonoBehaviour
+{
+    [SerializeField]
+    Text boneText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AreaUnlockTracker.BonePickupCounterIncreased += BonePickedUp;
+    }
+
+    void BonePickedUp(int boneCount)
+    {
+        boneText.text = boneCount.ToString();
+    }
+}
